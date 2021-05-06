a069febb397080d7d61004dc8a7ecb9359420667
Reece Kidd <rkidd@Reeces-MacBook-Pro.local>
2021-05-06 09:10:56 +0100

    chore: updated config files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
-}
+};
