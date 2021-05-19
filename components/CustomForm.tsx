8795d9a9baa59de34390f6f58a182fd8821901a8
Reece Kidd <reecekidd95@gmail.com>
2021-05-19 08:38:24 +0100

    refactor: Added CustomForm component

---
+++ components/CustomForm.tsx
@@ -0,0 +1,72 @@
+import { TextField, Button, makeStyles } from '@material-ui/core'
+import { Form, Formik } from 'formik'
+import * as React from 'react'
+
+interface Values {
+  activity: string
+  title: string
+  background: string
+}
+
+interface Props {
+  onSubmit: (values: Values) => void
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}))
+
+export const CustomForm: React.FC<Props> = ({ onSubmit }): JSX.Element => {
+  const classes = useStyles()
+  return (
+    <Formik
+      initialValues={{ activity: '', title: '', background: '' }}
+      onSubmit={(values) => {
+        onSubmit(values)
+      }}
+    >
+      {({ values, handleChange, handleBlur }) => (
+        <Form className={classes.root}>
+          <div>
+            <TextField
+              placeholder="Activity"
+              name="activity"
+              value={values.activity}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div>
+            <TextField
+              placeholder="Title"
+              name="title"
+              value={values.title}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div>
+            <TextField
+              placeholder="Background"
+              name="background"
+              value={values.background}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </div>
+          <div>
+            <Button variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+          </div>
+
+          <pre>{JSON.stringify(values, null, 2)}</pre>
+        </Form>
+      )}
+    </Formik>
+  )
+}
--- pages/become-a-coach.tsx
+++ pages/become-a-coach.tsx
@@ -1,26 +1,17 @@
-import React, { useState } from 'react'
-import { Container, Typography, Box, TextField, Button } from '@material-ui/core'
-
-import { CoachInput, useAddCoachMutation } from 'lib/graphql/AddCoach.graphql'
-import { useRouter } from 'next/router'
+import React from 'react'
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { Box, Container, Typography } from '@material-ui/core'
+import { useRouter } from 'next/router'
+
+import { CustomForm } from 'components/CustomForm'
+import { useAddCoachMutation } from 'lib/graphql/AddCoach.graphql'
 
 export default function BecomeACoach(): JSX.Element {
   const { user } = useUser()
-  const [activity, setActivity] = useState('')
-  const [title, setTitle] = useState('')
-  const [background, setBackground] = useState('')
+
   const router = useRouter()
 
   const [addCoach] = useAddCoachMutation()
-
-  const input: CoachInput = { activities: [activity], name: user.name, title, background }
-
-  const onSubmit = async (event): Promise<void> => {
-    event.preventDefault()
-    await addCoach({ variables: { input } })
-    router.push(`/coaches?activity=${activity}`)
-  }
 
   return (
     <Container maxWidth="lg">
@@ -28,37 +19,16 @@ export default function BecomeACoach(): JSX.Element {
         <Typography variant="h4" component="h1" gutterBottom>
           Become a coach
         </Typography>
-        <Box m={10}>
-          <form onSubmit={onSubmit} noValidate autoComplete="off">
-            <Box m={2}>
-              <TextField
-                id="filled-basic-2"
-                label="What do you want to coach?"
-                onChange={(e) => setActivity(e.target.value)}
-              />
-            </Box>
-
-            <Box />
-            <Box m={2}>
-              <TextField
-                id="filled-basic-2"
-                label="Your title"
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Box>
-            <Box m={2}>
-              <TextField
-                id="filled-basic-2"
-                label="Your background"
-                onChange={(e) => setBackground(e.target.value)}
-              />
-            </Box>
-
-            <Button type="submit" variant="contained" color="primary">
-              Sign up
-            </Button>
-          </form>
-        </Box>
+        <CustomForm
+          onSubmit={async ({ activity, title, background }) => {
+            await addCoach({
+              variables: {
+                input: { activities: [activity], name: user && user.name, title, background },
+              },
+            })
+            router.push(`/coaches?activity=${activity}`)
+          }}
+        />
       </Box>
     </Container>
   )
