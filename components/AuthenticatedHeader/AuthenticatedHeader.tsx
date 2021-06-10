2564e01a647eb1db8a52be0e36181398eb67e1d3
Reece Kidd <reecekidd95@gmail.com>
2021-06-10 08:47:20 +0100

    refactor: updated authenticated user header

--- components/AuthenticatedHeader/AuthenticatedHeader.tsx
+++ components/AuthenticatedHeader/AuthenticatedHeader.tsx
@@ -10,16 +10,17 @@ import {
   MenuItem,
   IconButton,
   Avatar,
+  Menu,
 } from '@material-ui/core'
-import SettingsIcon from '@material-ui/icons/Settings'
 import AccountCircle from '@material-ui/icons/AccountCircle'
+import MoreIcon from '@material-ui/icons/MoreVert'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useCurrentUserQuery, UserMode } from '../../lib/graphql/CurrentUser.graphql'
 import { useUpdateCurrentUserMutation } from '../../lib/graphql/UpdateCurrentUser.graphql'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  grow: {
     flexGrow: 1,
   },
   menuButton: {
@@ -28,8 +29,21 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-  list: {
-    width: 250,
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
   },
 }))
 
@@ -50,6 +64,107 @@ export default function AuthenticatedHeader({
 
   const [profilePicture, setProfilePicture] = useState('')
   const [mode, setMode] = useState(currentUser?.mode || '')
+  const [anchorElement, setAnchorElement] = useState(null)
+  const [mobileMoreAnchorElement, setMobileMoreAnchorElement] = useState(null)
+
+  const isMenuOpen = Boolean(anchorElement)
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorElement)
+
+  const handleMenuClose = (): void => {
+    setAnchorElement(null)
+    handleMobileMenuClose()
+  }
+  const handleMobileMenuClose = (): void => {
+    setMobileMoreAnchorElement(null)
+  }
+  const handleMobileMenuOpen = (event): void => {
+    setMobileMoreAnchorElement(event.currentTarget)
+  }
+
+  const profileIcon = currentUser && (
+    <IconButton
+      color="inherit"
+      onClick={() =>
+        currentUser.mode === UserMode.Coach
+          ? router.push('/coaching-dashboard')
+          : router.push('/student-dashboard')
+      }
+    >
+      {profilePicture ? <Avatar src={profilePicture} alt="User profile " /> : <AccountCircle />}
+    </IconButton>
+  )
+
+  const switchToCoachingLink = currentUser && mode === UserMode.Student && (
+    <Link href={'/coaching-dashboard'}>
+      <Button
+        color="inherit"
+        onClick={() => {
+          setMode(UserMode.Coach)
+          updateCurrentUser({ variables: { input: { mode: UserMode.Coach } } })
+        }}
+      >
+        {'Switch to coaching'}
+      </Button>
+    </Link>
+  )
+
+  const switchToStudentLink = currentUser && mode === UserMode.Coach && (
+    <Link href={'/student-dashboard'}>
+      <Button
+        color="inherit"
+        onClick={() => {
+          setMode(UserMode.Student)
+          updateCurrentUser({ variables: { input: { mode: UserMode.Student } } })
+        }}
+      >
+        {'Switch to student'}
+      </Button>
+    </Link>
+  )
+
+  const logoutLink = currentUser && (
+    <Link href={'/api/auth/logout'}>
+      <Button color="inherit">{'Logout'}</Button>
+    </Link>
+  )
+
+  const menuItems = [switchToCoachingLink, switchToStudentLink, profileIcon, logoutLink]
+    .filter((link) => link)
+    .map((element, index) => <MenuItem key={index}>{element}</MenuItem>)
+
+  const mobileMenuItems = [profileIcon, switchToCoachingLink, switchToStudentLink, logoutLink]
+    .filter((link) => link)
+    .map((element, index) => <MenuItem key={index}>{element}</MenuItem>)
+
+  const menuId = 'primary-search-account-menu'
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorElement}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      {menuItems}
+    </Menu>
+  )
+
+  const mobileMenuId = 'primary-search-account-menu-mobile'
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorElement}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      {mobileMenuItems}
+    </Menu>
+  )
 
   useEffect(() => {
     setProfilePicture(currentUser?.picture)
@@ -57,38 +172,9 @@ export default function AuthenticatedHeader({
   }, [currentUser])
 
   const router = useRouter()
-  const links = [
-    currentUser &&
-      mode === UserMode.Student && {
-        label: 'Switch to coaching',
-        href: '/coaching-dashboard',
-        onClick: () => {
-          setMode(UserMode.Coach)
-          updateCurrentUser({ variables: { input: { mode: UserMode.Coach } } })
-        },
-      },
-    currentUser &&
-      mode === UserMode.Coach && {
-        label: 'Switch to student',
-        href: '/student-dashboard',
-        onClick: () => {
-          setMode(UserMode.Student)
-          updateCurrentUser({ variables: { input: { mode: UserMode.Student } } })
-        },
-      },
-    currentUser && { label: 'Logout', href: '/api/auth/logout' },
-  ]
-    .filter((link) => link)
-    .map(({ label, href, onClick }) => (
-      <Link href={href} key={href}>
-        <Button color="inherit" onClick={onClick}>
-          {label}
-        </Button>
-      </Link>
-    ))
 
   return (
-    <div className={classes.root}>
+    <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
@@ -105,27 +191,22 @@ export default function AuthenticatedHeader({
             </Link>
           </Typography>
           <Switch checked={darkState} onChange={handleThemeChange} />
-          {links}
-          {currentUser && (
-            <MenuItem
-              onClick={() =>
-                currentUser.mode === UserMode.Coach
-                  ? router.push('/coaching-dashboard')
-                  : router.push('/student-dashboard')
-              }
+          <div className={classes.sectionDesktop}>{menuItems}</div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
             >
-              <IconButton color="inherit">
-                {profilePicture ? (
-                  <Avatar src={profilePicture} alt="User profile " />
-                ) : (
-                  <AccountCircle />
-                )}
-              </IconButton>
-            </MenuItem>
-          )}
-          <SettingsIcon onClick={() => router.push('/settings')} />
+              <MoreIcon />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
     </div>
   )
 }
